16064676077e4a201dc28c6b3729dba2e36f33b3
KubikNoLego <originallikitun@gmail.com>
2026-04-20 09:42:02 +0300

    🔁 Исправлен баг при открытии карт. Беслпатные открытия больше не обновляют время последнего открытия

--- app/services/random_card.py
+++ app/services/random_card.py
@@ -79,7 +79,6 @@ async def open_card(session: AsyncSession, user_id):
                         user.last_open.tzinfo is None else user.last_open)
         cooldown_hours = COOLDOWN - (1 if now.weekday() >= 5 else 0)
 
-        # Проверяем, можно ли открыть карту
         can_open = False
         if user.free_open > 0:
             can_open = True
@@ -112,12 +111,10 @@ async def open_card(session: AsyncSession, user_id):
             user.clan_member.contribution += clan_bonus
             user.clan_member.clan.balance += clan_bonus
 
-        # Уменьшаем free_open на 1, если есть бесплатные открытия
         if user.free_open > 0:
             user.free_open -= 1
-
-        # Обновляем время последнего открытия
-        user.last_open = now
+        else:
+            user.last_open = now
 
         await redis_requests.remove_luck_boost(user.id)
         await session.commit()
